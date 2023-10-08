955d99694797c6feac8758c6a149c0aa87544ac0
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2023-10-08 23:16:57 +0900

    Difficulty enum added

---
+++ Sources/KoreanTyperSharedDTO/Difficulty.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Seungsub Oh on 2023/10/08.
+//
+
+import Foundation
+
+enum Difficulty: String, Codable {
+    case easy = "Easy", 
+         normal = "Normal",
+         hard = "Hard",
+         expert = "Expert",
+         expertplus = "Expert+"
+}
