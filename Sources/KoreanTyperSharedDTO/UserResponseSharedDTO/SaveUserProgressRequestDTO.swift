11cdeaa4d69545608468f2854dd391a79fda4907
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2023-11-16 23:58:47 +0900

    user id is not needed for user progress save

--- Sources/KoreanTyperSharedDTO/UserResponseSharedDTO/SaveUserProgressRequestDTO.swift
+++ Sources/KoreanTyperSharedDTO/UserResponseSharedDTO/SaveUserProgressRequestDTO.swift
@@ -3,11 +3,9 @@ import Foundation
 public struct SaveUserProgressRequestDTO: Codable {
     public let traceTypeStageId: UUID?
     public let bombWordsStageId: UUID?
-    public let userId: UUID
     
     public init(traceTypeStageId: UUID?, bombWordsStageId: UUID?, userId: UUID) {
         self.traceTypeStageId = traceTypeStageId
         self.bombWordsStageId = bombWordsStageId
-        self.userId = userId
     }
 }
