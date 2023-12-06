e824fc459152105a66ab2d47d74a006c06f8c08a
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2023-12-06 21:50:36 +0900

    score argument added - SaveUserProgressRequestDTO

--- Sources/KoreanTyperSharedDTO/UserProgressSharedDTO/SaveUserProgressRequestDTO.swift
+++ Sources/KoreanTyperSharedDTO/UserProgressSharedDTO/SaveUserProgressRequestDTO.swift
@@ -3,9 +3,11 @@ import Foundation
 public struct SaveUserProgressRequestDTO: Codable {
     public let traceTypeStageId: UUID?
     public let bombWordsStageId: UUID?
+    public let score: Float
     
-    public init(traceTypeStageId: UUID?, bombWordsStageId: UUID?) {
+    public init(traceTypeStageId: UUID?, bombWordsStageId: UUID?, score: Float) {
         self.traceTypeStageId = traceTypeStageId
         self.bombWordsStageId = bombWordsStageId
+        self.score = score
     }
 }
