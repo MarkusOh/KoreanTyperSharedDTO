74c39462379adec17f9c97f5c54035eddae2bfc4
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2023-12-07 13:50:39 +0900

    UserProgressResponse type-o fix

--- Sources/KoreanTyperSharedDTO/UserProgressSharedDTO/UserProgressResponseDTO.swift
+++ Sources/KoreanTyperSharedDTO/UserProgressSharedDTO/UserProgressResponseDTO.swift
@@ -2,12 +2,12 @@ import Foundation
 
 public struct UserProgressResponseDTO: Codable {
     public let id: UUID
-    public let TraceTypeStageScores: [StageScoreResponseDTO]
-    public let BombWordsStageScores: [StageScoreResponseDTO]
+    public let traceTypeStageScores: [StageScoreResponseDTO]
+    public let bombWordsStageScores: [StageScoreResponseDTO]
     
-    public init(id: UUID, TraceTypeStageScores: [StageScoreResponseDTO], BombWordsStageScores: [StageScoreResponseDTO]) {
+    public init(id: UUID, traceTypeStageScores: [StageScoreResponseDTO], bombWordsStageScores: [StageScoreResponseDTO]) {
         self.id = id
-        self.TraceTypeStageScores = TraceTypeStageScores
-        self.BombWordsStageScores = BombWordsStageScores
+        self.traceTypeStageScores = traceTypeStageScores
+        self.bombWordsStageScores = bombWordsStageScores
     }
 }
